29589980b4945ab33800275ffbf1fb4989822702
Rakshanda wankhade <rakshandawankhade95@gmail.com>
2022-10-07 18:13:29 +0530

    local storage

--- Task11_store_to_local_storage/main.js
+++ Task11_store_to_local_storage/main.js
@@ -9,14 +9,3 @@ function saveToLocalStorage(event) {
   };
   localStorage.setItem(obj.email, JSON.stringify(obj));
 }
-
-window.addEventListener("DOMContentLoaded", () => {
-  const localStorageObj = localStorage;
-  const localStoragekeys = Object.keys(localStorageObj);
-
-  for (var i = 0; i < localStoragekeys.length; i++) {
-    const key = localStoragekeys[i];
-    const userDetailsString = localStorageObj[key];
-    const userDetailsObj = JSON.parse(userDetailsString);
-  }
-});
